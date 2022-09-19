5212c1f13cf58a2a98a8f97a3acb8e4ea1443381
Infinti <firatahmetyonat@gmail.com>
2022-09-19 16:00:46 +0300

    12 vid sonu

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,10 +1,14 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,13 +26,13 @@ namespace Business.Concrete
             _productDal = productDal;
         }//end injection
 
+        [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
-            if (product.ProductName.Length <=2)
-            {
-                //magic strings hata mesajlarının metot sınıflara dönüştürülmesi
-                return new ErrorResult(Messages.ProductNameInvalid);
-            }
+            //businnes code=iş ihtiyaçlarına uygunluk
+            //validation code=nesnenin yapısal olarak uygunluğunu kontrol eden yapı
+            //birbirleri yerine koyma!
+
             _productDal.Add(product);
 
             return new SuccessResult(Messages.ProductAdded);//constr a hemen bilgi verilip işlem burada oluşturulup yapılır
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,23 +1,35 @@
-﻿using Business.Abstract;
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Business.Abstract;
 using Business.Concrete;
+using Business.DependencyResolvers.Autofac;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFrameWork;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//5.0 dan 60 a geçiş aşağıdaki kod
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+    .ConfigureContainer<ContainerBuilder>(builder =>
+    {
+        builder.RegisterModule(new AutofacBusinessModule());
+    });
 
 builder.Services.AddControllers();
-builder.Services.AddSingleton<IProductService,ProductManager>();//new leme IoC direkt manager oluşturur. yani resolve hatası giderilir
+////builder.Services.AddSingleton<IProductService,ProductManager>();//new leme IoC direkt manager oluşturur. yani resolve hatası giderilir
 //Singleton içinde data tutulmazsa kullanılır
 //ödev AddScoped,AddTransient
-builder.Services.AddSingleton<IProductDal,EfProductDal>();
+////builder.Services.AddSingleton<IProductDal,EfProductDal>();
 
 //Autofac,Ninject v.. gibi yapılar otomatik IoC yapısı kurar ve kullanır
+//12. videoda autofac kullanıldı ve yukarıdaki 2 singleton oraya taşındı**
+
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
